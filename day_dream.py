e1706378adb4a077c2f7379af1b81d3d4b9148a9
1617jangheyungjungit config --global user.name 1617jangheyungjun <jangh4139@gmail.com>
2022-09-11 17:06:01 +0900

    의사와 유저 충돌 버그 수정

--- day_dream.py
+++ day_dream.py
@@ -1,19 +1,24 @@
 #불러오기
-from http import server
-from http.cookiejar import CookieJar
-import secrets
-import sys
-from turtle import back
 import pygame
-
+import sys
 #변수불러오기
 map_lotate = 0
 Communicate = 0
+right_x_crash = 0
+left_x_crash = 0
+up_y_crash = 0
+down_y_crash = 0
 #아이콘 그림 가져오기
 game_icon = pygame.image.load("icon\\icon.png")
 #배경 그림 가져오기
 background = pygame.image.load("bg_image\\test.png")
 sub_background = pygame.image.load("bg_image\\2test.png")
+#책상 그림 가져오기
+first_desk = pygame.image.load("object\\desk.png")
+first_desk_size = first_desk.get_rect().size
+first_desk_width = first_desk_size[0]
+first_desk_height = first_desk_size[1]
+desk = 0
 # 게임구성
 pygame.init()
 fps = 60
@@ -36,8 +41,8 @@ doctor_character = pygame.image.load("character_image\\sub_dummy.png")
 doctor_character_size = doctor_character.get_rect().size
 doctor_character_width = doctor_character_size[0]
 doctor_character_height = doctor_character_size[1]
-doctor_x_pos = width / 2 + width / 4
-doctor_y_pos = height / 2
+doctor_x_pos = width / 2 + width / 3
+doctor_y_pos = height / 2 - doctor_character_height /2
 
 #의사와 주인공 상호작용
 doctor_meet_main = False
@@ -61,7 +66,11 @@ while  True:
         screen.blit(sub_background, (0, 0))
 
         screen.blit(main_character, (main_x_pos, main_y_pos))
-    
+    #책상위치 가져오기
+    if map_lotate == 0:
+        desk_x_pos = 1280
+        desk_y_pos = height / 2 - first_desk_height / 2
+        screen.blit(first_desk, (desk_x_pos, desk_y_pos))
     for event in pygame.event.get():
         #게임 종료
         if event.type == pygame.KEYDOWN:
@@ -70,19 +79,19 @@ while  True:
                 sys.exit()
         #주인공 캐릭터 움직임
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a and right_x_crash == 0:
                 if main_to_x == 5:
                     main_to_x = 0
                 main_to_x -= 5
-            elif event.key == pygame.K_d:
+            elif event.key == pygame.K_d and left_x_crash == 0:
                 if main_to_x == -5:
                     main_to_x = 0
                 main_to_x += 5
-            elif event.key == pygame.K_w:
+            elif event.key == pygame.K_w and down_y_crash == 0:
                 if main_to_y == 5:
                     main_to_y = 0
                 main_to_y -= 5
-            elif event.key == pygame.K_s:
+            elif event.key == pygame.K_s and up_y_crash == 0:
                 if main_to_y == -5:
                     main_to_y = 0
                 main_to_y += 5
@@ -114,9 +123,16 @@ while  True:
         main_y_pos = height / 2
         Communicate = 1
         doctor_meet_main = True
+    #의사 클래스 제작
+    class doctor(pygame.sprite.Sprite):
+        def __init__(self):#클래스 초기화
+            pygame.sprite.Sprite.__init__(self) #스프라이트 초기화
+            self.image = pygame.image.load("character_image\\sub_dummy.png")
+            self.image = pygame.transform.scale(self.image, (doctor_character_width, doctor_character_height))
+            self.rect = self.image.get_rect()
+            self.rect.centerx = doctor_x_pos
+            self.rect.centery = doctor_y_pos
 
-    if map_lotate == 0 and doctor_x_pos - main_x_pos > 64 and doctor_y_pos - main_y_pos > 64:
-        doctor_meet_main = False
 
     if map_lotate == 0 and doctor_meet_main == False:
         print("의사와 대화해 보자")
@@ -125,6 +141,12 @@ while  True:
         print("서사")
         Communicate = 0
     
+    
+    #책상 못지나가게
+    if map_lotate == 0 and desk_x_pos - main_x_pos == 0:
+        left_crash = 1
+    if map_lotate == 0 and desk_x_pos - main_x_pos == -first_desk_width:
+        right_crash = 1
 #게임반복 뒤
 
     pygame.display.flip()
